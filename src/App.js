c565fd346ad3593c12f4793820dc9028092d0ad9
root <root@ip-172-31-1-108.us-west-1.compute.internal>
2020-09-28 08:48:37 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hi Kalyan...how r u...this is react js app
         </p>
         <a
           className="App-link"
